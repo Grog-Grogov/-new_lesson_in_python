1ffec29e3bcc2fff171ddf63af3b975375587b1b
Grog <groggrogov@yandex.ru>
2022-07-31 12:31:52 +0300

    les_31_07_click_but

--- les_31_07.py
+++ les_31_07.py
@@ -6,3 +6,12 @@ print(new_len(4,6))
 
 asd = lambda a,b,c,d:a + b + c + d
 print(asd(4,7,9,3))
+print(new_len(44,77))
+
+
+lambda a,b,c,v:a + b + c + v
+print(lambda a,b,c,v:a + b + c + v)
+
+a = [(1, 12), (4, 1), (9, 10), (13, -3)]
+a.sort(key=lambda x: x[1])
+print(a)
